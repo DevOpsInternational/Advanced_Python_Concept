e0689bfd217fe5a0d02a322432527acc02764df4
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-11 21:35:12 +0530

    Used created SimMath Module

---
+++ ImportedModule.py
@@ -0,0 +1,8 @@
+import SimMath as sm
+a=10
+b=2
+print("Addition of a and b is: ",sm.ad(a,b))
+print("Subtraction of a and b is: ",sm.sb(a,b))
+print("Multiplication of a and b is: ",sm.ml(a,b))
+print("Division of a and b is: ",sm.dv(a,b))
+print("Power Function on a and b is: ",sm.pw(a,b))
