8734bf22aa170510fac52a013defd1da61b3da7e
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-14 18:50:33 +0530

    Trying to use assert statement

---
+++ UseAssert.py
@@ -0,0 +1,3 @@
+#use of assert statements
+x=10
+assert x<10,"Here x should be less than 10 but not"
---
+++ assertUse.py
@@ -0,0 +1,7 @@
+def squarIt(x):
+    return x**2
+assert squarIt(2)=4,"The square of 2 should be 4"
+print(squarIt(2))
+print(squarIt(4))
+print(squarIt(8))
+
