8ed0960df2ec50662ef24909a883ea898a6e5f49
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-14 18:36:42 +0530

    Logging module programs

---
+++ ExceptionLogging.py
@@ -0,0 +1,15 @@
+#sending exception information to logging file
+import logging as lg
+lg.basicConfig(filename="LogginG.txt")
+lg.info("New request came,we welcome you")
+try:
+    a=int(input("Enter value of one number"))
+    b=int(input("Enter value of another number"))
+    print(a/b)
+except ZeroDivisionError as msg:
+    print("You cannot divide number with zero")
+    lg.exception(msg)
+except ValueError as msg:
+    print("Enter proper value of numbers")
+    lg.exception(msg)
+lg.info("Request of mathematical calculation in processed")
