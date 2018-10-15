45c3524cee8c15837bc549077550b9aaf8c29d7b
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-15 17:41:00 +0530

    CSV file Reading

---
+++ CSVReadingFile.py
@@ -0,0 +1,13 @@
+#reading data to csv file
+import csv
+with open("emp.csv","r") as f:
+    r=csv.reader(f)
+    data=list(r)
+    print(type(data))
+##    print("EmpName","\t","EmpEdd")
+    for i in data:
+        for j in i:
+            print(j,"\t",end="")
+        print()
+            
+#print("Data entered of {} number of employees".format(n))
