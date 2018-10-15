cd21ad5ddc8bfd71c5d85facfa3797f8a70ee70e
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-15 22:12:59 +0530

    OOPs concept programming

---
+++ OOP1.py
@@ -0,0 +1,6 @@
+#oops programming
+class Student:
+    def read(self):
+        print("I am method in Class Student and my task is reading")
+s=Student()
+s.read()
---
+++ OOP2.py
@@ -0,0 +1,9 @@
+#oops programming
+class Student:
+    '''This is current class with required data'''
+    def read(self):
+        print("I am method in Class Student and my task is reading")
+s=Student()
+s.read()
+print(s.__doc__)
+##help(Student)
