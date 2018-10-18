9513cab994a50d5525ff449ec5b5fd301606dcf7
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-18 22:23:09 +0530

    Modification

--- SuperMethod.py
+++ SuperMethod.py
@@ -3,6 +3,7 @@ class Person:
     def __init__(self,name,age):
         self.name=name
         self.age=age
+        self.x=10
     def h(self):
         print("For software engineer python learning is very easy")
 class SE(Person):
@@ -10,6 +11,7 @@ class SE(Person):
         super().__init__(name,age)
         self.eno=eno
         self.esal=esal
+##        print(super().x) invalid
     def disp(self):
         print("Python is very easy for SW People")
 s=SE("Tejaswita",28,189,100000)
